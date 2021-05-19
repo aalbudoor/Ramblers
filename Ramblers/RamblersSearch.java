7cfeac19ad89e76ca2d7c15289cc99a3a8fa72b1
aalbudoor <aalbudoor@abdullas-mbp>
2021-05-20 03:35:52 +0400

    committing RamblersSearch

---
+++ Ramblers/RamblersSearch.java
@@ -0,0 +1,45 @@
+public class RamblersSearch extends Search {
+
+	RamblerState init;
+	RamblerState goal;
+	SearchNode initNode;
+	SearchNode goalNode;
+	
+	public RamblersSearch()
+	{
+		TerrainMap tmap = new TerrainMap("/Users/aalbudoor/Downloads/Ramblers/tmc.pgm");
+		init = new RamblerState(tmap.getTmap(),tmap.getWidth(),tmap.getDepth(),0,0);
+		goal = new RamblerState(tmap.getTmap(),tmap.getWidth(),tmap.getDepth(),tmap.getWidth()-1,tmap.getDepth()-1);
+		initNode = new SearchNode(init,0,0);
+		goalNode = new SearchNode(goal,0,0);
+		
+		
+	}
+	
+	public RamblersSearch(RamblerState init,RamblerState goal)
+	{
+		
+		this.init = init;
+		this.goal = goal;
+		initNode = new SearchNode(init,0,0);
+		goalNode = new SearchNode(goal,0,0);
+	}
+	
+	public void runSearch(String strat)
+	{
+		super.runSearch(init,strat);
+	}
+
+	
+	public SearchNode getGoal()
+	{
+		return this.goalNode;
+	}
+	
+//	public static void main(String args[])
+//	{
+//		RamblersSearch search = new RamblersSearch();
+//		search.runSearch("branchAndBound");
+//	}
+//	
+}
